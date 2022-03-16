d0a864f7f6e3aa9ca42f3c51c62a72bb0dcef19a
juli6464 <juli6464@hotmail.com>
2022-03-16 17:41:32 -0500

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
-module.exports = nextConfig
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    mode: 'production',
+    disable: false,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/],
+  },
+  reactStrictMode: true,
+  images: {
+    domains: ['placeimg.com'],
+  },
+});
---
+++ src/containers/ProductList.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ProductItem from '@components/ProductItem';
+import useGetProducts from '@hooks/useGetProducts';
+import styles from '@styles/ProductList.module.scss';
+
+const API = 'https://api.escuelajs.co/api/v1/products';
+
+// const ProductList = () => {
+//   const products = useGetProducts(API);
+const ProductList = ({product}) => {
+	const products = useGetProducts(API);
+
+//   const ProductItem = ({ product }) => {
+// 	const { state, addToCart } = useContext(AppContext);
+  
+
+  
+	// if(!product.images[0]){
+	//   return null;
+	// }
+
+
+  return (
+    <section className={styles['main-container']}>
+      <div className={styles.ProductList}>
+        {products?.map((product) => (
+          <ProductItem product={product} key={product.id} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ProductList;
